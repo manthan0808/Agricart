4992db97d7f4cdc84cfaa303836aa8fb64a987ca
manthan0808 <patelmanthan0808@gmail.com>
2026-04-09 00:37:11 +0530

    Fix Vercel build and untrack local binaries

--- start.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set PHP_BIN=C:\Agricart\php\php.exe
-set PORT=8080
-
-echo Starting Agricart on http://localhost:%PORT%...
-echo Press Ctrl+C to stop the server.
-
-"%PHP_BIN%" -S localhost:%PORT% -t .
-pause
